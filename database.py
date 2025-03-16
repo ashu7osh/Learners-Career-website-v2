7e5faabb7fe477d601e380df866527317e5de9b8
ashutosh <ashutoshjatav19@gmail.com>
2025-03-16 20:41:17 +0530

    Added a seprate jobpage template, and did some modification in the previous code

--- database.py
+++ database.py
@@ -16,3 +16,15 @@ def load_jobs_from_db():
         result = conn.execute(text("SELECT * FROM jobs"))
         jobs = [row._asdict() for row in result]  # Convert Row to dictionary
     return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val": id}  # Pass parameters as a dictionary
+        )
+        rows = result.all()
+        if len(rows ) == 0:
+            return None 
+        else:
+            return rows[0]._asdict()  # Convert Row object to dictionary
